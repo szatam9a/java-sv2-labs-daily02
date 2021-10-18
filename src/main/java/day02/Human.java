135373009bc2bfa674b880cd20430889da5f66d4
Szalay Tamás <szalaytamas90@gmail.com>
2021-10-18 15:44:55 +0200

    Human class kész projekt start

---
+++ src/main/java/day02/Human.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class Human {
+    private String name;
+    private String age;
+
+    public Human(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
